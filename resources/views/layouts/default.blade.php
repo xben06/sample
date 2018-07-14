0fbbaba662958e128722189100175f3f1c041d19
xben06 <632173221@qq.com>
2018-07-14 10:42:08 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>@yield('title','Sample') - Laravel 新手入门教程</title>
-  </head>
-  <body>
-    @yield('content')
-  </body>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Sample App') --Laravel入门教程</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+</body>
 </html>
