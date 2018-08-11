f701b684042651bf90407bd3e7916e65e85bae2a
xben06 <632173221@qq.com>
2018-08-11 10:11:43 +0000

    add weibo

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,9 +26,13 @@ class UsersController extends Controller
     public function create(){
     	return view('users.create');
     }
-    public function show(User $user){
-    	return view('users.show',compact('user'));
-    }
+	public function show(User $user)
+	{
+		$statuses = $user->statuses()
+			->orderBy('created_at', 'desc')
+			->paginate(30);
+		return view('users.show', compact('user', 'statuses'));
+	}
 	public function store(Request $request)
 	{
 		$this->validate($request, [
